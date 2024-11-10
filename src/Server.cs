8dbb17bb20148ea4f8ad2369fde9a947ef94d337
TanvirSojal <tanvirahmedsojal@gmail.com>
2024-11-10 22:33:27 +0600

    [any message]

--- src/Server.cs
+++ src/Server.cs
@@ -8,20 +8,28 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-var acceptedSocket = server.AcceptSocket(); // wait for client
 
-var buffer = new byte[256];
-var bytesReceived = 0;
-var totalBytesReceived = 0;
-var data = "";
+while(true){
+    var acceptedSocket = await server.AcceptSocketAsync(); // wait for client
+    Console.WriteLine("Accepted a new connection");
+    _ = Task.Run(async () => await HandleIncomingRequestAsync(acceptedSocket));
+}
 
-var response = Encoding.UTF8.GetBytes("+PONG\r\n");
+async Task HandleIncomingRequestAsync(Socket socket){
+    Console.WriteLine("Handling request");
+    var buffer = new byte[256];
+    var bytesReceived = 0;
+    var totalBytesReceived = 0;
+    var data = "";
 
-while((bytesReceived = await acceptedSocket.ReceiveAsync(buffer)) > 0){
-    totalBytesReceived += bytesReceived;
-    data += Encoding.UTF8.GetString(buffer);
-    //Console.WriteLine(data, bytesReceived);
-    await acceptedSocket.SendAsync(response);
+    var response = Encoding.UTF8.GetBytes("+PONG\r\n");
+
+    while((bytesReceived = await socket.ReceiveAsync(buffer)) > 0){
+        totalBytesReceived += bytesReceived;
+        data += Encoding.UTF8.GetString(buffer);
+        //Console.WriteLine(data, bytesReceived);
+        await socket.SendAsync(response);
+    }
 }
 
 // Console.WriteLine(data);
