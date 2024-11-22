fba9332af9e2c70b8ad2007b588c9ca164fb7971
TanvirSojal <tanvirahmedsojal@gmail.com>
2024-11-23 02:03:12 +0600

    update logging

--- src/Logger.cs
+++ src/Logger.cs
@@ -2,11 +2,16 @@ public static class Logger
 {
     public static void Log(string? message)
     {
-        Console.WriteLine($"{DateTime.UtcNow} | thread_id: [{Thread.CurrentThread.ManagedThreadId}] {message}");
+        Console.WriteLine($"{GetCurrentDateTime()} | thread_id: [{Thread.CurrentThread.ManagedThreadId}] {message}");
     }
 
     public static void Log(object? value)
     {
-        Console.WriteLine($"{DateTime.UtcNow} | thread_id: [{Thread.CurrentThread.ManagedThreadId}] {value}");
+        Console.WriteLine($"{GetCurrentDateTime()} | thread_id: [{Thread.CurrentThread.ManagedThreadId}] {value}");
+    }
+
+    private string GetCurrentDateTime()
+    {
+        return DateTime.UtcNow.ToString("HH:mm:ss.fff");
     }
 }
